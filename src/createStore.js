c8cef3b8dfaa8a41527dbfcad008313edff680d1
Suchitra Naik <susanaik@gmail.com>
2018-10-18 14:29:42 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -20,6 +20,6 @@ export default function createStore(reducer) {
 
 function render() {
   const container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState.count;
 
 }
