9dbef0aac502b86030496e372aae4024a62e92fd
Suchitra Naik <susanaik@gmail.com>
2018-10-18 14:34:24 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -6,30 +6,17 @@ export default function createStore(reducer) {
     return state;
   };
 
-
   function dispatch(action) {
-    state = changeCount(state, action);
-    render();
-  };
+      render();
+    }
 
   return {
-    dispatch,
     getState
    };
 }
 
-function changeCount(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-
-    default:
-      return state;
-  }
-};
 
 function render() {
   const container = document.getElementById('container');
-  container.textContent = store.getState().count;
 
 }
